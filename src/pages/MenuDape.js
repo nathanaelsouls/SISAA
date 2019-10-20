574b1755fe5e2242871cc596cbcfee86141f5834
Nathanael <nathanaelotaku28@gmail.com>
2019-10-20 17:52:38 -0300

    Ajustanto icones

--- src/pages/MenuDape.js
+++ src/pages/MenuDape.js
@@ -20,13 +20,13 @@ return (
         <View style={styles.colunasMenu}>            
         <TouchableOpacity style={styles.colunasBotao}>
             <TouchableOpacity onPress={() => this.props.navigation.navigate('Formulario400Hz')} style={styles.menuButton} >
-            <Image style={{ width: '50%', height: '50%' }} source={require('../imagens/energy.png')}/>
+            <Image style={{ width: '60%', height: '50%' }} source={require('../imagens/energy.png')}/>
             <Text/>
             <Text style={styles.buttonText}>400 Hz</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => this.props.navigation.navigate('FormularioAmbulift')} style={styles.menuButton} >
-            <Image style={{ width: '53%', height: '50%' }} source={require('../imagens/ambulift.png')}/>
+            <Image style={{ width: '60%', height: '50%' }} source={require('../imagens/ambulift.png')}/>
             <Text/>
             <Text style={styles.buttonText}>Ambulift</Text>                
             </TouchableOpacity>
@@ -35,12 +35,12 @@ return (
         <View style={styles.colunasMenu}>            
         <TouchableOpacity style={styles.colunasBotao}>                
             <TouchableOpacity onPress={() => this.props.navigation.navigate('FormularioTripulanteDesembarque')} style={styles.menuButton} >
-            <Image style={{ width: '50%', height: '50%' }} source={require('../imagens/landing.png')}/>
+            <Image style={{ width: '60%', height: '50%' }} source={require('../imagens/landing.png')}/>
             <Text style={styles.buttonText}>Tripulante e/ou Desembarque Híbrido</Text>
             </TouchableOpacity>
             
             <TouchableOpacity onPress={() => this.props.navigation.navigate('FormularioLavagemTecnica')} style={styles.menuButton} >
-            <Image style={{ width: '50%', height: '50%' }} source={require('../imagens/wash.png')}/>            
+            <Image style={{ width: '60%', height: '50%' }} source={require('../imagens/wash.png')}/>            
             <Text/>
             <Text style={styles.buttonText}>Lavagem Técnica</Text>                            
             </TouchableOpacity>
